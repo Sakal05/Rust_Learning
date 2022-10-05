175b2d150cb86c23ee2fecb536a721c19e49707e
Sakal <samnang.sakal@gmail.com>
2022-10-05 09:48:46 +0700

    init

---
+++ Hello-world/hello-world.rs
@@ -0,0 +1,12 @@
+
+fn main() {
+    println!("Hello World!");
+
+    test_switch(12);
+}
+
+// place holder perform like c
+fn test_switch(opt: i32) {
+    println!("The value of opt is: {opt}");
+}
+
