f93abe9099d30ec884ff928ec758acec6a4596f5
Sakal <samnang.sakal@gmail.com>
2022-10-05 16:36:55 +0700

    print and datatype

--- Hello-world/hello-world.rs
+++ Hello-world/hello-world.rs
@@ -1,12 +1,44 @@
 
 fn main() {
-    println!("Hello World!");
 
-    test_switch(12);
+    // /* Placeholder */
+    // //printing hello world
+    // println!("Hello World!");
+
+    // //positional placeholder perform likes C
+    // println!("The value of opt is: {opt}", opt = 12);
+
+    // //placeholder traits
+    // println!("Number : 10 \nBinary:{:b} Hexadecimal:{:x} Octal:{:o}", 10, 10, 10);
+
+    // //placeholdr for a Debug trait
+    // println!("{:?}", ("This is a Rust Course", 101, "Sakal", 999));
+
+    // /* Printing Type */
+    // // Print!() use to print string onto a console
+    // print!("Testing hello world");
+    // print!("\n");
+    // //println!() same as print!() just append new line at the end of the string
+    // println!("Hello world");
+    // println!("Hello world on new line");
+
+    // //eprint!() displays the outputas an error
+    // eprint!("This is an error");
+
+    /* variable declaration */
+    let x = 12;
+    println!("The number is {}", x);
+    
+    //to let varible mutable we must use "mut" before variable name
+    let mut mut_x = 12;
+    println!("The number that mut is {}", mut_x);
+    mut_x = 99;
+    println!("The number after mutable is {}", mut_x);
+
+    //assigne multiple variable 
+    let (id, name) = (1, "Sakal");
+    println!("{}. {}", id, name);
 }
 
-// place holder perform like c
-fn test_switch(opt: i32) {
-    println!("The value of opt is: {opt}");
-}
+
 
